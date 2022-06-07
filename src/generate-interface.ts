c07ccae4f825cb075b058745507830bd45be61da
Trias <triasbrata@gmail.com>
2022-06-08 05:23:02 +0700

    wip

--- src/generate-interface.ts
+++ src/generate-interface.ts
@@ -7,6 +7,7 @@ import { basename, dirname, join } from "path";
 import { execShell } from "./utils";
 import { execSync } from "child_process";
 import { snakeCase } from "change-case";
+import lineColumn = require("line-column");
 export const generateInterfaceCmd = vscode.commands.registerCommand(
   "golangx.implementGen",
   async (uri: vscode.Uri) => {
@@ -16,7 +17,13 @@ export const generateInterfaceCmd = vscode.commands.registerCommand(
       const goplsBinPath = "$GOPATH/bin/gopls";
       //get init path
 
-      const interfaceCont = fs.readFileSync(uri.fsPath).toString();
+      const textEditorInterface = await vscode.workspace.openTextDocument(uri);
+      const interfaceCont = textEditorInterface.getText();
+      const editor = await vscode.window.showTextDocument(
+        textEditorInterface,
+        1,
+        true
+      );
       const gomodPaths = await vscode.workspace.findFiles("go.mod", "", 1);
       if ((gomodPaths?.length || 0) < 1) {
         vscode.window.showErrorMessage("go.mod not found");
@@ -33,13 +40,12 @@ export const generateInterfaceCmd = vscode.commands.registerCommand(
         vscode.window.showErrorMessage("module not define in go.mod");
         return;
       }
-      const interfaces = interfaceCont
-        .match(/type ([a-zA-z]+) interface/g)
-        ?.map((it) => it.replace(/(type|interface|\s)/g, ""))
-        .filter((it) => it.length > 0);
+      const iname = Array.from(
+        interfaceCont.matchAll(/^type ([\w\d]+) interface {(?:.*?|\n)*?}/gm)
+      ).map(([, iName]) => iName);
 
-      if (interfaces && interfaces.length > 0) {
-        if (interfaces.length > 1) {
+      if (iname && iname.length > 0) {
+        if (iname.length > 1) {
           vscode.window.showErrorMessage("cant handle 2 interface in 1 file");
           return;
         }
@@ -47,7 +53,7 @@ export const generateInterfaceCmd = vscode.commands.registerCommand(
         const regExpPackageInterface = /^package\s(\w+)/.exec(interfaceCont);
         if (!regExpPackageInterface || regExpPackageInterface.length < 2) {
           vscode.window.showErrorMessage(
-            `interface ${interfaces[0]} dont have package`
+            `interface ${iname[0]} dont have package`
           );
           return;
         }
@@ -63,11 +69,11 @@ export const generateInterfaceCmd = vscode.commands.registerCommand(
         pathPackInterface = pathPackInterface.endsWith(packageInterface)
           ? pathPackInterface
           : join(baseModule, packageInterface);
-        const interfaceName = interfaces[0];
+        const interfaceName = iname[0];
         const structName = interfaceName.toLowerCase();
         const structType = `${structName[0]} *${structName}`;
         const implFolder = path.join(uri.fsPath, "..", "impl");
-        if (fs.existsSync(implFolder)) {
+        if (!fs.existsSync(implFolder)) {
           fs.mkdirSync(implFolder, {
             recursive: true,
           });
@@ -91,32 +97,97 @@ export const generateInterfaceCmd = vscode.commands.registerCommand(
             )
           );
         }
-        const cmd = `cd ${dirname(
-          uri.fsPath
-        )} && ${implBinPath} "${structType}" ${interfaceName}`;
+        const regexMethodWantImpl = interfaceCont.matchAll(
+          /^\s([\w\d]+)(\(([\s\w\d+\,\.]*)\))( (\(([\s\w\d+\,\.]*)\)|[\s\w\d]+))?/gm
+        );
         //genereate impl
-        console.log(cmd);
-        const listImpl = execSync(cmd).toString();
-        const methRegex = /func\s\(.*\)\s(\w+).*\n.*\n.*/gm;
-        for (const [content, name] of listImpl.matchAll(methRegex)) {
-          const uriImpMeth = vscode.Uri.file(
-            join(implFolder, `${snakeCase(name)}.go`)
+        const selections: vscode.Selection[] = [];
+        for (const item of regexMethodWantImpl) {
+          const [full, methodName, inFull, inOnly, _, outFull, outOnly] = item;
+          const methodIndex = item.index!;
+          const rangeStart = textEditorInterface.positionAt(methodIndex);
+          const rangeEnd = textEditorInterface.positionAt(
+            methodIndex + full.length
           );
-          const exists = fs.existsSync(uriImpMeth.fsPath);
-          if (!exists) {
-            wsedit.createFile(uriImpMeth, {
-              overwrite: true,
-              ignoreIfExists: false,
+          // selections.push(new vscode.Selection(rangeStart, rangeEnd));
+
+          const inArgs = inOnly?.split(",").map((it) => it.trim());
+          const inImport = inArgs
+            ?.filter((it) => it.indexOf(".") != -1)
+            .map((it) => {
+              const [_, typeIn] = it.split(" ");
+              return typeIn;
             });
-            wsedit.insert(
-              uriImpMeth,
-              new vscode.Position(0, 0),
-              methodContent(content)
-            );
-          } else {
-            fs.readFileSync(uriImpMeth.fsPath).toString();
+          const outArgs = outOnly?.split(",").map((it) => it.trim());
+          const outImport = outArgs
+            ?.filter((it) => it.indexOf(".") != -1)
+            .map((it) => {
+              const [_, typeIn] = it.split(" ");
+              return typeIn;
+            });
+          if (inImport.length > 0) {
+            const paramIndex = full.indexOf(inOnly);
+            console.log(full);
+            for (const inImp of inImport) {
+              const inImpIdx = inOnly.indexOf(inImp);
+              const posStart = methodIndex + paramIndex + inImpIdx;
+              const rangeStartParam = textEditorInterface.positionAt(posStart);
+              const rangeEndParam = textEditorInterface.positionAt(
+                posStart + inImp.length
+              );
+              selections.push(
+                new vscode.Selection(rangeStartParam, rangeEndParam)
+              );
+            }
           }
         }
+        editor.selections = selections;
+
+        // const methRegex =
+        //   /^func \([\w\s\d+\*]+\) ([\w\d]+)(\(([\s\w\d+\,\.]*)\))(\s\((.*)\)|\s[\w\d]+)? {\s.*\s}$/gm;
+        // const methContent = Array.from(listImpl.matchAll(methRegex));
+        // for (const mc of methContent) {
+        //   console.log(mc.input?.indexOf(mc[2]), mc[2]);
+        // }
+        // console.log({ methContent });
+        const listImpFile = fs.readdirSync(implFolder);
+        const methodImplementeds = listImpFile.reduce(
+          (methodImplemented, filename) => {
+            const filePath = join(implFolder, filename);
+            const methSymbols = execSync(
+              `cd ${dirname(filePath)} && ${goplsBinPath} symbols ${filename}`
+            )
+              .toString()
+              .matchAll(/.*\.(.*) Method \d+:\d+-\d+:\d+$/gm);
+            return [
+              ...methodImplemented,
+              ...Array.from(methSymbols).map(([, methodName]) => methodName),
+            ];
+          },
+          [] as string[]
+        );
+        // for (const [content, name] of [[]]) {
+        //   if (methodImplementeds.includes(name)) {
+        //     continue;
+        //   }
+        //   const uriImpMeth = vscode.Uri.file(
+        //     join(implFolder, `${snakeCase(name)}.go`)
+        //   );
+        //   const exists = fs.existsSync(uriImpMeth.fsPath);
+        //   if (!exists) {
+        //     wsedit.createFile(uriImpMeth, {
+        //       overwrite: true,
+        //       ignoreIfExists: false,
+        //     });
+        //     wsedit.insert(
+        //       uriImpMeth,
+        //       new vscode.Position(0, 0),
+        //       methodContent(content)
+        //     );
+        //   } else {
+        //     fs.readFileSync(uriImpMeth.fsPath).toString();
+        //   }
+        // }
         if (wsedit.entries().length > 0) {
           vscode.workspace.applyEdit(wsedit);
         }
